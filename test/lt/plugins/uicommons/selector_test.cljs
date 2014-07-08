2ea750c739bdd24fa3b802669d6cb8924fa7281c
ndr <ndr@qef.io>
2014-07-08 03:02:02 +0200

    Add pseudotest for multimode-selector

--- test/lt/plugins/uicommons/selector_test.cljs
+++ test/lt/plugins/uicommons/selector_test.cljs
@@ -41,3 +41,25 @@
               :options exec-selector
               :exec (fn [item]
                       (println "Exec selector: " item))})
+
+
+;;;; test multimode-selector
+
+(def test-modes [{:key :key
+                  :button-text "Key"
+                  :default true}
+                 {:key :a
+                  :button-text "a"}
+                 {:key :b
+                  :button-text "b"}])
+
+(def multimode-selector (sel/multimode-selector {:key :key
+                                                 :modes test-modes
+                                                 :placeholder "multimode-selector"
+                                                 :items (fn [] items)
+                                                 :transform selector-transform}))
+
+(cmd/command {:command ::test-multimode-selector
+              :desc "UICommons: test multimode-selector"
+              :options multimode-selector
+              :exec (fn [item] )})
