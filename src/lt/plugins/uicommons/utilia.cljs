acc4df91253b0c9de615e89d2b0ab1b47bd747f3
ndr <ndr@qef.io>
2014-07-14 23:25:58 +0200

    Define deep-merge for objects

--- src/lt/plugins/uicommons/utilia.cljs
+++ src/lt/plugins/uicommons/utilia.cljs
@@ -2,10 +2,13 @@
   (:require [lt.object :as object]
             [lt.util.dom :as dom]))
 
-(defn deep-merge [& vals]
-  (if (every? map? vals)
-    (apply merge-with deep-merge vals)
-    (last vals)))
+(defn deep-merge [& xs]
+  (if (every? map? xs)
+    (apply merge-with deep-merge xs)
+    (last xs)))
+
+(defn deep-merge! [obj & xs]
+  (swap! obj deep-merge xs))
 
 (defn ^boolean atom? [x]
   (satisfies? IDeref x))
@@ -17,3 +20,6 @@
 
 (defn append-obj [parent child]
   (dom/append (->content parent) (->content child)))
+
+(defn cssel [& xs]
+  (keyword (reduce str (map name xs))))
