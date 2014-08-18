d8e218457a80e63867d4e2ee985e4a4bad3420db
ndr <ndr@qef.io>
2014-08-18 11:58:36 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject light-uicommons "0.0.1-SNAPSHOT"
+(defproject light-uicommons "0.0.2-SNAPSHOT"
   :description "Reusable, extensible UI components for Light Table"
 
   :dependencies [[org.clojure/clojure "1.5.1"]])
